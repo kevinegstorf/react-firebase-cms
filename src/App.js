bcd4c97fe423924971e51303def6b977dbb00a68
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-05 23:20:35 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Admin from './containers/Admin'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Admin/>
+    );
+  }
+}
+
+export default App;
