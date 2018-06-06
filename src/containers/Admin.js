98fe92dfe1d1ae750ab16dd283e7c9da595fbaac
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-06 14:59:35 +0200

    refactored admin comp wkith validity checker

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -3,6 +3,7 @@ import Card from "@material-ui/core/Card";
 import styles from "./Admin.css";
 import Button from "../components/UI/Button/Button";
 import Input from "../components/UI/Input/Input";
+import validityChecker from "../utils/validityChecker";
 
 export default class Admin extends Component {
   state = {
@@ -35,37 +36,6 @@ export default class Admin extends Component {
     formIsValid: false
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    return isValid;
-  }
-
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.controls
@@ -77,7 +47,7 @@ export default class Admin extends Component {
 
     updatedFormElement.value = event.target.value;
     console.log("updatedFormElement", updatedFormElement.value);
-    updatedFormElement.valid = this.checkValidity(
+    updatedFormElement.valid = validityChecker(
       updatedFormElement.value,
       updatedFormElement.validation
     );
