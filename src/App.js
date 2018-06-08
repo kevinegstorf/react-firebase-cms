e436f2ab25561c2eac58a094c5a50f29d6479358
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-08 13:14:18 +0200

    added animation for login card

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import Admin from "./containers/Admin/Admin";
 import CMSForm from "./containers/CMSForm/CMSForm";
 import HomePage from "./containers/HomePage/HomePage";
 
-const browserHistory = "";
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Router history={browserHistory}>
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/admin" exact component={Admin} />
-            <Route path="/cms" exact component={CMSForm} />
-          </Switch>
-        </Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/admin" exact component={Admin} />
+          <Route path="/cms" exact component={CMSForm} />
+        </Switch>
       </div>
     );
   }
