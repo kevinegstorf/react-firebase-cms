43bc7a723c9705c963eaebfbbf79b5e108f7f243
kevinegstorf <kevin.egstorf@gmail.com>
2018-06-10 00:12:11 +0200

    added tests for props

--- src/components/UI/Button/Button.test.js
+++ src/components/UI/Button/Button.test.js
@@ -5,13 +5,27 @@ import Adapter from "enzyme-adapter-react-16";
 
 import Button from "./Button";
 
-// const props = { variant="contained", disabled= 'disabled', onClick='clicked' };
 configure({ adapter: new Adapter() });
+const wrapper = shallow(<Button />);
+
+wrapper.setProps({
+  variant: "contained",
+  disabled: "disabled"
+});
 
 describe("<Button>", () => {
   it("should be defined", () => {
     expect(Button).toBeDefined();
   });
 
-  // TODO test props
+  it("should be rendered with the correct props", () => {
+    // console.log(wrapper.debug());
+    // console.log(wrapper.props().variant);
+    expect(wrapper.props().variant).toBeDefined();
+    expect(wrapper.props().disabled).toBeDefined();
+
+    expect(wrapper.prop("variant")).toEqual("contained");
+    expect(wrapper.prop("disabled")).toEqual("disabled");
+  });
+  // todo test click event
 });
